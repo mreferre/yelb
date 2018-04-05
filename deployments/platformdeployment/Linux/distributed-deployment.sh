b86c77d3b121d323bea94a01816b30aa8dadef2b
Massimo Re Ferre <massimo@it20.info>
2018-04-05 21:50:00 +0200

    This commit (and the previous "test" commit) add bash and cloudformation scripts to install yelb on Linux instances

--- deployments/platformdeployment/Linux/distributed-deployment.sh
+++ deployments/platformdeployment/Linux/distributed-deployment.sh
@@ -6,13 +6,13 @@
 # Run this script on the redis-server instance:
 
 #!/bin/bash
-curl https://gist.githubusercontent.com/mreferre/46613d0e89af8527130ba5008ed18974/raw/yelb-redis.sh | bash 
+curl https://raw.githubusercontent.com/mreferre/yelb/master/deployments/platformdeployment/Linux/redis-server.sh | bash 
 
 
 # Run this script on the yelb-db instance:
 
 #!/bin/bash
-curl https://gist.githubusercontent.com/mreferre/2001b99ed163103bf4c90aee87be1c7b/raw/yelb-db.sh | bash
+curl https://raw.githubusercontent.com/mreferre/yelb/master/deployments/platformdeployment/Linux/yelb-db.sh | bash
 
 
 # Run this script on the yelb-appserver instance:
@@ -20,12 +20,12 @@ curl https://gist.githubusercontent.com/mreferre/2001b99ed163103bf4c90aee87be1c7
 #!/bin/bash
 export REDIS_SERVER_ENDPOINT=IP/FQDN-redis
 export YELB_DB_SERVER_ENDPOINT=IP/FQDN-db
-curl https://gist.githubusercontent.com/mreferre/27c85452ea264f25ed1f63952d3cf61b/raw/yelb-appserver.sh | bash
+curl https://raw.githubusercontent.com/mreferre/yelb/master/deployments/platformdeployment/Linux/yelb-appserver.sh | bash
 
 
 # Run this script on the yelb-ui instance:
 
 #!/bin/bash
 export YELB_APPSERVER_ENDPOINT=IP/FQDN-appserver
-curl https://gist.githubusercontent.com/mreferre/09d298c53d28d99c4924c405425b4b8b/raw/yelb-ui.sh | bash
+curl https://raw.githubusercontent.com/mreferre/yelb/master/deployments/platformdeployment/Linux/yelb-ui.sh | bash
 
