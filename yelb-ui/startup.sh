c17bd96353ef7d25e4c3974b67201bb1329d2d7a
Massimo Re Ferre <massimo@it20.info>
2018-10-20 10:55:51 +0200

    Bumped Node version in the yelb-ui container. Also added logic in startup.sh files to populate resolv.conf to allow for proper DNS resolution when used with selected service discovery solutions

--- yelb-ui/startup.sh
+++ yelb-ui/startup.sh
@@ -2,6 +2,10 @@
 NGINX_CONF=/etc/nginx/conf.d/default.conf
 cd clarity-seed
 
+# when the variable is populated a search domain entry is added to resolv.conf at startup
+# this is needed for the ECS service discovery (a search domain can't be added with awsvpc mode) 
+if [ $SEARCH_DOMAIN ]; then echo "search ${SEARCH_DOMAIN}" >> /etc/resolv.conf; fi 
+
 sed -i -- 's#/usr/share/nginx/html#/clarity-seed/'$UI_ENV'/dist#g' $NGINX_CONF
 
 # this adds the reverse proxy configuration to nginx 
