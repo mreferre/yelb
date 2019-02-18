9f6815ead5461ca9464f04527ae6b2bbc1736d82
Massimo Re Ferre <massimo@it20.info>
2019-02-18 12:28:27 +0100

    Fixed typo in a Lambda adapter

--- yelb-appserver/getvotes_adapter.rb
+++ yelb-appserver/getvotes_adapter.rb
@@ -3,7 +3,7 @@ require_relative 'modules/getvotes'
 def getvotes_adapter(event:, context:)
     $yelbdbhost = ENV['yelbdbhost']
     $yelbdbport = 5432
-    votes = getVotes()
+    votes = getvotes()
     # use the return JSON command when you want the API Gateway to manage the http communication  
     # return JSON.parse(votes)
     { statusCode: 200, body: votes }
