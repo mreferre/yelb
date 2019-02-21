dfaa6bd4b97cf984ec49af03dc634b874bac023c
Massimo Re Ferre <massimo@it20.info>
2019-02-21 11:53:09 +0100

    Support to compile yelb-ui "once" and deploy "many" by tweaking the env.js file (at deployment time) with the yelb-appserver endpoint url

--- yelb-ui/clarity-seed-newfiles/src/app/app.component.ts
+++ yelb-ui/clarity-seed-newfiles/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from '../environments/environment';
+import { EnvService } from '../app/env.service';
 import { Headers, Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -15,10 +16,23 @@ import 'rxjs/add/operator/map';
 
 export class AppComponent implements OnInit {
     constructor(private router: Router,
-                private http:  Http
+                private http:  Http,
+                private env: EnvService
                ) {}
 
+/** Here we set the appserver endpoint. 
+If you are using the nginx redirect method (used in the bare metal, EC2 and containers deployment) leave it as is:
+>> public appserver    =    environment.appserver_env;
+If you want to take advantage of the env.js file (used in the serverless S3 static web hosting use case) then change it to:
+>> public appserver    =    this.env.apiUrl;
+This will set the endpoint to the value found in env.js (right now this change needs to be done at build time e.g. via sed)
+For reference: support for reading the env.js file has been introduced following these steps: 
+https://www.jvandemo.com/how-to-use-environment-variables-to-configure-your-angular-application-without-a-rebuild/ 
+*/
+
 public appserver = environment.appserver_env;
+
+
 colorScheme = {
     domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
     };
