49105585cc56d386645b336278bc60092f9f20b7
Massimo Re Ferre <massimo@it20.info>
2020-05-14 18:01:20 +0000

    Added the proxy_http_version 1.1 directive to the nginx configuration (required by App Mesh) + elevated the node version

--- yelb-ui/startup.sh
+++ yelb-ui/startup.sh
@@ -11,11 +11,15 @@ sed -i -- 's#/usr/share/nginx/html#/clarity-seed/'$UI_ENV'/dist#g' $NGINX_CONF
 # this adds the reverse proxy configuration to nginx 
 # everything that hits /api is proxied to the app server     
 if ! grep -q "location /api" "$NGINX_CONF"; then
-	echo "    location /api {" > /proxycfg.txt
-	echo "        proxy_pass http://yelb-appserver:4567/api;" >> /proxycfg.txt
-	# echo "        proxy_set_header Host $host;" >> /proxycfg.tx
-	echo "    }" >> /proxycfg.txt
-	sed --in-place '/server_name  localhost;/ r /proxycfg.txt' $NGINX_CONF
+    eval "cat <<EOF
+    location /api {
+        proxy_pass http://yelb-appserver:4567/api;
+        proxy_http_version 1.1;
+    }
+EOF
+" > /proxycfg.txt
+    # echo "        proxy_set_header Host $host;" >> /proxycfg.txt
+    sed --in-place '/server_name  localhost;/ r /proxycfg.txt' $NGINX_CONF
 fi
 nginx -g "daemon off;" 
 
