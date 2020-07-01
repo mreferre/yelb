c55189fe8bae0fb1e17dbf75bd3106cea966a3ba
Massimo Re Ferre <massimo@it20.info>
2020-07-01 11:03:06 +0200

    Expanded the in-line comments to explain why the need of $SEARCH_DOMAIN hack

--- yelb-ui/startup.sh
+++ yelb-ui/startup.sh
@@ -3,7 +3,9 @@ NGINX_CONF=/etc/nginx/conf.d/default.conf
 cd clarity-seed
 
 # when the variable is populated a search domain entry is added to resolv.conf at startup
-# this is needed for the ECS service discovery (a search domain can't be added with awsvpc mode) 
+# this is needed for the ECS service discovery given the app works by calling host names and not FQDNs
+# a search domain can't be added to the container when using the awsvpc mode 
+# and the awsvpc mode is needed for A records (bridge only supports SRV records) 
 if [ $SEARCH_DOMAIN ]; then echo "search ${SEARCH_DOMAIN}" >> /etc/resolv.conf; fi 
 
 sed -i -- 's#/usr/share/nginx/html#/clarity-seed/'$UI_ENV'/dist#g' $NGINX_CONF
