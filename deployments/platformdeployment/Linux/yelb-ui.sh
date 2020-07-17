3f01b617acb5fe6d26a6520c6ac3afd2f9d8623f
Massimo Re Ferre <massimo@it20.info>
2020-07-17 11:11:14 +0200

    Bumped Node version from 10.9 to 10.13 (due to a dependency with the Angular CLI) in the yelb-ui Linux script

--- deployments/platformdeployment/Linux/yelb-ui.sh
+++ deployments/platformdeployment/Linux/yelb-ui.sh
@@ -47,7 +47,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 . ~/.nvm/nvm.sh
-nvm install 10.9
+nvm install 10.13
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
