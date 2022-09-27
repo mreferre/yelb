f901897ffd996cf531db2e4c3f777f9385b4d227
Massimo Re Ferre <massimo@it20.info>
2022-09-27 14:21:52 +0200

    Fixed the redis policy for remote connectivity

--- deployments/platformdeployment/Linux/redis-server.sh
+++ deployments/platformdeployment/Linux/redis-server.sh
@@ -18,5 +18,7 @@ fi
 amazon-linux-extras install epel -y
 yum update -y
 yum install redis -y
+sed -i "s/bind 127.0.0.1/bind 0.0.0.0/" /etc/redis.conf
+sed -i "s/protected-mode yes/protected-mode no/" /etc/redis.conf
 systemctl enable redis
 systemctl start redis
