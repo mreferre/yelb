f9361a60ecfcf2eca587fc056fa8a19e576ae32a
Massimo Re Ferre <massimo@it20.info>
2022-09-27 10:29:34 +0200

    Fixed redis script

--- deployments/platformdeployment/Linux/redis-server.sh
+++ deployments/platformdeployment/Linux/redis-server.sh
@@ -15,21 +15,8 @@ if [ ! -d $HOMEDIR ]; then
 fi 
 ###########################################################
 
-cd $HOMEDIR
-yum -y install gcc64 gcc-c++ make jemalloc-devel
-wget http://download.redis.io/redis-stable.tar.gz
-tar xvzf redis-stable.tar.gz
-cd redis-stable
-make
-cp src/redis-server /usr/bin/
-cp src/redis-cli /usr/bin/
-#the script below configures the redis service
-REDIS_PORT=6379 \
-    REDIS_CONFIG_FILE=/etc/redis/6379.conf \
-    REDIS_LOG_FILE=/var/log/redis_6379.log \
-    REDIS_DATA_DIR=/var/lib/redis/6379 \
-    REDIS_EXECUTABLE=`command -v redis-server` ./utils/install_server.sh
-#the commands below configure redis to accept remote connections
-service redis_6379 stop
-sed -i "s/bind 127.0.0.1/bind 0.0.0.0/" /etc/redis/6379.conf
-service redis_6379 start
+amazon-linux-extras install epel -y
+yum update -y
+yum install redis -y
+systemctl enable redis
+systemctl start redis
